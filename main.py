8c057c14bc20a2d6f46e7039df64e85d800f882c
olcoste <olcoste@f2r7s17.paris.42.school>
2026-09-11 18:01:41 +0200

    lin

--- main.py
+++ main.py
@@ -4,55 +4,48 @@ from math_formula import ft_normalisation
 def ft_estimate_price(theta0: int, theta1: int, mileage: int) -> int:
 	return theta0 + (theta1 * mileage)
 
+def save_data(iteration: int, theta0: int, theta1: int):
+
+	data_lst: lst = [['iteration', iteration + 1],
+					['theta0', theta0],
+					['theta1', theta1]]
+
+	#print(data_lst)
+
+	with open('test.csv', 'a', newline='') as csvfile:
+		writer = csv.writer(csvfile, delimiter=' ')
+		writer.writerows(data_lst)
+		writer.writerow([])
+
 def calculation(mileage_price_list: list):
 	theta0: int = 0
 	theta1: int = 0
-	#mileage: int = mileage_price_list[0][0]
-	#price: int = mileage_price_list[1][0]
+	iteration: int = 1000
 	learning_rate: int = 0.01
 	mileage_lst = ft_normalisation(mileage_price_list[0])
 	price_lst =  ft_normalisation(mileage_price_list[1])
-	theta0_tmp: int = 0
-	theta1_tmp = int = 0
+	theta0_tmp: float = 0
+	theta1_tmp = float = 0
 
-	for x in range(100):
+	for x in range(iteration):
 		for index, (price, mileage) in enumerate(zip(price_lst, mileage_lst)):
-			
 			estimate_price = ft_estimate_price(theta0, theta1, mileage)
-			#print("theta0", theta0)
-			#print("theta1", theta1)
-			#print("mileage", mileage)
-			#print("estimate_price", estimate_price)
-			#print("estimate_price", estimate_price)
-
 
 			theta0_tmp += (estimate_price - price)
-			#print("theta0_tmp", estimate_price - price)
-			#theta0_tmp = learning_rate * ((estimate_price - price))# / len(mileage_price_list[0]))
-			#print(theta0_tmp)
-			#theta0 = theta0_tmp
 
 			theta1_tmp += ((estimate_price - price) * mileage)
-			#print("theta1_tmp", (estimate_price - price) * mileage)		
-			#print()
 
 
-			#theta1_tmp = learning_rate * (((estimate_price - price) * mileage))#/ len(mileage_price_list[0]))
-			#print(theta1_tmp)
-			#theta1 = theta1_tmp
-			#print()
 			if index == len(price_lst) - 1:
 				theta0 = learning_rate * (theta0_tmp / len(price_lst))
-				print("theta0", theta0)
+				#print("theta0", theta0)
 				theta1 += -(learning_rate * (theta1_tmp / len(price_lst)))
-				print("theta1", theta1)
+				#print("theta1", theta1)
 				theta0_tmp = 0
 				theta1_tmp = 0
-				#print("theta0", theta0)
-				#print("theta1", theta1)
-				print()
+				save_data(x, theta0,theta1)
 				#print()
-				#print()
+	print("theta0", theta0, "  theta1", theta1)
 
 def extract_data() -> list: #verif a faire liste vide, element egeuax
 	mileage_price_list = [], []
